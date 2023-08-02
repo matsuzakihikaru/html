53dfd2aea8fd2b9a920c2dcd0130e1e0e9465828
matsuzakihikaru <matsuzakihikaru00@gmail.com>
2023-08-02 15:35:39 +0900

    20230802

--- hack_exp/Display.js
+++ hack_exp/Display.js
@@ -3,28 +3,30 @@
 class Display {
 
 	draw() {
-		let turn = screen;
+		let turn = screen%20;
+		let pass = Math.floor(screen / 20);
 
-		context.clearRect(0, 0, 1500, 7000);
+		context.clearRect(0, 0, 1500, 700);
 
         context.font = "24px sans-serif";
         context.fillText("条件を考えてください " +(turn+1)+" / 4枚目 ", 0, 128*3+24);
+        context.fillText((pass+1)+" / 12 パスワード ", 0, 128*3+48);
 
 		//画像描画
 	    for (let i = 0; i < 9; i++) {
 	        let img = new Image();
-	        if (data[0][3][turn] - 1 == i) {
-	        	img.src = '../images/synthesized/'+data[0][1][turn];
+	        if (data[pass][3][turn] - 1 == i) {
+	        	img.src = '../images/synthesized/'+data[pass][1][turn];
 
 	        }
 	        else {
-	        	img.src = '../images/synthesized/'+data[0][2][turn*9+i];
+	        	img.src = '../images/synthesized/'+data[pass][2][turn*9+i];
 	        }
 	        
 	        img.onload = function(){
 	            context.drawImage(img, i%3*imgsize, Math.floor(i/3)*imgsize, imgsize, imgsize);
 
-	        if (data[0][3][turn] - 1 == i) {
+	        if (data[pass][3][turn] - 1 == i) {
 	        	context.strokeStyle = "red";
 		        context.lineWidth = 6;
 		        context.strokeRect(i%3*imgsize, Math.floor(i/3)*imgsize, imgsize, imgsize);
--- hack_exp/Hack.js
+++ hack_exp/Hack.js
@@ -3,22 +3,24 @@
 class Hack {
 
 	draw() {
-		let turn = screen - 10 + 4;//turnは4以上
+		let turn = screen%20;//turnは4以上
+		let pass = Math.floor(screen / 20);
 
-		context.clearRect(0, 0, 1500, 7000);
+		context.clearRect(0, 0, 1500, 700);
 
         context.font = "24px sans-serif";
-        context.fillText("推測した条件を満たす画像をクリックしてください " +(turn+1 - 4)+" / 4枚目 ", 0, 128*3+24);
+        context.fillText("推測した条件を満たす画像をクリックしてください " +(turn-3)+" / 8枚目 ", 0, 128*3+24);
+        context.fillText((pass+1)+" / 12 パスワード ", 0, 128*3+48);
 
 		//画像描画
 	    for (let i = 0; i < 9; i++) {
 	        let img = new Image();
-	        if (data[0][3][turn] - 1 == i) {
-	        	img.src = '../images/synthesized/'+data[0][1][turn];
+	        if (data[pass][3][turn] - 1 == i) {
+	        	img.src = '../images/synthesized/'+data[pass][1][turn];
 
 	        }
 	        else {
-	        	img.src = '../images/synthesized/'+data[0][2][turn*9+i];
+	        	img.src = '../images/synthesized/'+data[pass][2][turn*9+i];
 	        }
 	        
 	        img.onload = function(){
